275a1f4f3881873838d5ed452d0c458b8890be09
Hide Ziegler <hidez246@gmail.com>
2021-11-05 21:46:22 -0600

    stop balloon from going too high and bounces when hits ground

--- Challenge 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Challenge 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
+    // Variable
     public bool gameOver;
 
     public float floatForce;
     public float gravityModifier;
+    public float topBound = 14.0f;
     private Rigidbody playerRb;
 
     public ParticleSystem explosionParticle;
@@ -16,6 +18,7 @@ public class PlayerControllerX : MonoBehaviour
     private AudioSource playerAudio;
     public AudioClip moneySound;
     public AudioClip explodeSound;
+    public AudioClip bounceSound;
 
 
     // Start is called before the first frame update
@@ -33,6 +36,13 @@ public class PlayerControllerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Stops balloon from going too high
+        if (transform.position.y > topBound)
+        {
+            transform.position = new Vector3(transform.position.x, topBound, transform.position.z);
+            playerRb.velocity = Vector3.zero;
+            playerRb.angularVelocity = Vector3.zero;
+        }
         // While space is pressed and player is low enough, float up
         if (Input.GetKey(KeyCode.Space) && !gameOver)
         {
@@ -59,6 +69,13 @@ public class PlayerControllerX : MonoBehaviour
             playerAudio.PlayOneShot(moneySound, 1.0f);
             Destroy(other.gameObject);
 
+        }
+
+        // Makes balloon bounce when hitting the ground
+        else if (other.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerAudio.PlayOneShot(bounceSound, 1.0f);
         }
 
     }
